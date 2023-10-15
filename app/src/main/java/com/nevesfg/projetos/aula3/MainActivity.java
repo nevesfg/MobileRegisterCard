f7aeddb7494aea40304e8caf9e516197b9ab35a0
nevesfg <jaojose711@gmail.com>
2023-10-14 21:17:21 -0300

    Initial commit

---
+++ app/src/main/java/com/nevesfg/projetos/aula3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nevesfg.projetos.aula3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
